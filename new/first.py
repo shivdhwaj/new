6b1880625016cf749504f08fcd60c1308c2cd6c2
akanksha-singh <akanksha.singh@openlabs.co.in>
2013-02-28 16:21:13 +0530

    first comit

---
+++ new/first.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    :first
+
+    Description goes here...
+
+    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+
